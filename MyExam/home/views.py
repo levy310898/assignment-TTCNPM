8e6cc32dcc26d2835a6324e7d7c88b8facff46ed
tuandinhduc <tuandinhduc@gmail.com>
2020-06-13 10:48:55 +0700

    nothing

--- MyExam/home/views.py
+++ MyExam/home/views.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.shortcuts import render
-<<<<<<< HEAD
 from .forms import RegistrationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
@@ -8,9 +7,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-=======
 from .models import *
->>>>>>> 81369a2592a8be968b56ee138f6e43072dfc8f1a
 # Create your views here.
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
@@ -25,8 +22,6 @@ def login_request(request):
                 login(request, user)
                 messages.info(request, "You are now logged in as {username}")
                 return HttpResponseRedirect('/home/home')
-                
-    
             else:
                 messages.info(request, "Invalid username or password.")
         else:
@@ -36,7 +31,6 @@ def login_request(request):
 def home(request):
     return render(request,'home/home.html')
 
-<<<<<<< HEAD
 def signUp(request):
     form = RegistrationForm()
     if request.method == 'POST':
@@ -45,7 +39,6 @@ def signUp(request):
             form.save()
             return HttpResponseRedirect('/home/home')
     return render(request, 'home/sign-up.html', {'form': form})
-=======
 def myTest(request):
     return render(request,'home/my-test.html')
 
@@ -56,4 +49,3 @@ def doTest(request, exam_name):
         'question' : ques,
     }
     return render(request,'home/do-test.html',context)
->>>>>>> 81369a2592a8be968b56ee138f6e43072dfc8f1a
