4da4f179a7952f88200dde882f8877cfc843d6bc
levy310898 <levy310898@gmail.com>
2020-06-23 16:24:18 +0700

    update login form error and my-test data

--- MyExam/home/urls.py
+++ MyExam/home/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('', views.login_request, name = "sign-in"),
     path('home/user=<str:username>/', views.home, name= "home"),
     path('sign-up/', views.signUp, name= "sign-up"),
-    path('my-test/', views.myTest, name= "my-test"),
+    path('home/user=<str:username>/my-test/', views.myTest, name= "my-test"),
     path('home/user=<str:username>/exam=<str:exam_name>/', views.doTest, name= "do-test"),
     path('admin/',views.admin,name="admin"),
 
